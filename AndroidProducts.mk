9cd144bbf137d602b482d63623905db81d661761
HQJtiger <18948166799@163.com>
2026-04-24 14:36:48 +0800

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for ZTE ZTE A2020 SP (P865A02)
-
-```
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_P865A02.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_P865A02-user \
+    omni_P865A02-userdebug \
+    omni_P865A02-eng
