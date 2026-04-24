4dd7440385f10def8d6744ff892a45e564c884c4
HQJtiger <18948166799@163.com>
2026-04-24 19:26:37 +0800

    Add target arch/bootloader; remove recovery copy
    Populate BoardConfig.mk with target architecture and CPU variants (arm64 primary, arm secondary), CPU ABIs, CPU_VARIANT_RUNTIME, bootloader settings (TARGET_BOOTLOADER_BOARD_NAME, TARGET_NO_BOOTLOADER) and platform/recovery pixel format (TARGET_BOARD_PLATFORM=kona, TARGET_RECOVERY_PIXEL_FORMAT=RGBX_8888). Remove the PRODUCT_COPY_FILES block in device.mk that copied a prebuilt recovery ramdisk, since the prebuilt recovery copy is no longer required.

--- device.mk
+++ device.mk
@@ -6,9 +6,5 @@
 
 LOCAL_PATH := device/zte/P865A02
 
-# Prebuilt files for recovery ramdisk
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/recovery,$(TARGET_COPY_OUT_RECOVERY)/root)
-
 # Custom ROM asserts
 TARGET_OTA_ASSERT_DEVICE := P865A02,A2020SP,ZTE_A2020_SP
